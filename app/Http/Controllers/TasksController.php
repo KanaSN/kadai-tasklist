b238649156188539b15eed8a221db03c8768d267
Kana <kanako.sone@rakuten.com>
2018-06-05 04:49:43 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,12 +44,12 @@ public function create()
 public function store(Request $request)
     {
         $this->validate($request, [
-            'title' => 'required|max:191',
+            'status' => 'required|max:10',
             'content' => 'required|max:191',
         ]);
 
         $task = new Task;
-        $task->title = $request->title;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -68,12 +68,12 @@ public function store(Request $request)
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'title' => 'required|max:191',
+            'status' => 'required|max:191',
             'content' => 'required|max:191',
         ]);
 
         $task = Task::find($id);
-        $task->title = $request->title;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- database/migrations/2018_06_05_133225_add_status_to_tasks_table.php
+++ database/migrations/2018_06_05_133225_add_status_to_tasks_table.php
@@ -4,26 +4,29 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTitleToTasksTable extends Migration
+class AddStatusToTasksTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    
-public function up()
+    public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('title');
+            $table->string('status','10');
         });
     }
 
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('title');
+            //
         });
     }
 }
