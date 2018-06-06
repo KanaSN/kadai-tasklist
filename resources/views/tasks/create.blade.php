403a912e0f7be3954c58c4a77f893e5f6bd36430
Kana <kanako.sone@rakuten.com>
2018-06-06 07:22:51 +0000

    add bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
     <h1>タスク新規作成ページ</h1>
 
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12 col-sm-8-offset-2 col-md-8-offset-2 col-lg-3-offset-3">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
                 <div class="form-group">
